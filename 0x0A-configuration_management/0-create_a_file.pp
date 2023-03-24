8c52cf52c29974342ae1394a33913acffc4bc88c
waley-code <walephlp@gmail.com>
2023-03-24 11:12:21 +0000

    completed task 0

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Puppet manifest that creates the file with the specified requirements
+
+file { '/tmp/school':
+  ensure  => 'file',
+  content => 'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+}
