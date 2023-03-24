d712c8157e1a6bc5fa7cb378070ba9971e37a21e
waley-code <walephlp@gmail.com>
2023-03-24 11:44:41 +0000

    updated task 2

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,7 @@
 # Puppet manifest that kills a killmenow using the pkill command
 
 exec { 'killmenow':
+  path    => '/usr/bin/',
   command => 'pkill killmenow',
   onlyif  => 'pgrep killmenow',
 }
