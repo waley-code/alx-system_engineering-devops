01753781d54dab67ea1de95ad80d8ff50a807b3b
waley-code <walephlp@gmail.com>
2023-03-25 09:29:49 +0000

    add rsa path to puppet file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -7,8 +7,6 @@ file { '~/.ssh/config':
   mode    => '0600',
   content => "\
 Host 54.174.243.255
-  Hostname 54.174.243.255
-  User ubuntu
   IdentityFile ~/.ssh/id_rsa
   IdentityFile ~/.ssh/school
   PasswordAuthentication no"
