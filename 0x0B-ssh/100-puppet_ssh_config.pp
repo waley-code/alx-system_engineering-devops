82f8fe2489aaf6b0c5fd287edebf262ef8db839b
waley-code <walephlp@gmail.com>
2023-03-25 07:44:14 +0000

    create advanced puppet task

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+# Puppet to make changes to our configuration file
+
+file { '~/.ssh/config':
+  ensure  => 'file',
+  owner   => 'uduntu',
+  group   => 'ubuntu',
+  mode    => '0600',
+  content => "\
+Host 54.174.243.255
+  Hostname 54.174.243.255
+  User ubuntu
+  IdentityFile ~/.ssh/school
+  PasswordAuthentication no"
+}
