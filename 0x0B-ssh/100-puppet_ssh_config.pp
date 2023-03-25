79af8b8f8559295e5e1b5833777c63a3d5d81cc9
waley-code <walephlp@gmail.com>
2023-03-25 10:04:57 +0000

    updated advanced task

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,16 +1,17 @@
 # Puppet to make changes to our configuration file
-file { '/home/user/.ssh/config':
+file { '~/.ssh/config':
   ensure  => file,
   owner   => 'user',
   group   => 'user',
   mode    => '0600',
   content => "\
-Host <server_ip>
-  IdentityFile /home/user/.ssh/school
-  IdentityFile /home/user/.ssh/id_rsa
-  UserKnownHostsFile /dev/null
-  StrictHostKeyChecking no
-  PasswordAuthentication no
-  ForwardAgent yes
+Host 54.174.243.255
+    Hostname 54.174.243.255
+    IgnoreUnkown AddKeysToAgent,UseKeychain
+    UseKeychain yes
+    AddKeysToAgent yes
+    IdentityFile ~/.ssh/school
+    IdentityFile ~/.ssh/id_rsa
+    PasswordAuthentication no
 "
 }
