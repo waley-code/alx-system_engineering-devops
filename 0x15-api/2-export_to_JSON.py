0ca933b6446b25a1cf90f78d1582635ba936bfb8
waley-code <oyewaleadeosun@yahoo.com>
2023-04-24 23:15:39 +0100

    2-export_to_JSON.py

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Python script that, using this REST API,
+    Given employee ID, returns information
+    about his/her todo list progress.
+"""
+import requests
+import sys
+import json
+
+
+def funct():
+    """Displays tasks based on id argument from scriot"""
+
+    Id_us = 'https://jsonplaceholder.typicode.com/users/{}'.format(sys.argv[1])
+    Id_td = 'https://jsonplaceholder.typicode.com/todos/?userId={}'.format(sys.argv[1])
+    user_name = requests.get(Id_us).json().get('username')
+    req_todo = requests.get(Id_td).json()
+    texts = []
+
+    with open('{}.json'.format(sys.argv[1]), 'w+') as file:
+        for todo in req_todo:
+            text = {"task": todo.get("title"),
+                    "completed": todo.get("completed"), "username": user_name}
+            texts.append(text)
+        paxx = {Id_us: texts}
+
+        file.write(json.dumps(paxx))
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        funct()
