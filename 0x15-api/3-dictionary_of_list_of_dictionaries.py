47ff467208118d926bc2764a439a368d73187218
waley-code <oyewaleadeosun@yahoo.com>
2023-04-24 23:16:01 +0100

    3-dictionary_of_list_of_dictionaries.py

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" Python script that, using this REST API,
+    Given employee ID, returns information
+    about his/her todo list progress.
+"""
+import requests
+import sys
+import json
+
+
+def funct():
+    """Displays tasks based on id argument from scriot"""
+
+    Id_us = 'https://jsonplaceholder.typicode.com/users'
+    users = requests.get(Id_us).json()
+    info = {}
+    for user in users:
+        user_id = user.get('id')
+        name = user.get('username')
+        todos = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(
+            user_id)
+        request_todo = requests.get(todos).json()
+        tasks = []
+        for todo in request_todo:
+            task = {"username": name, "task": todo.get("title"),
+                    "completed": todo.get("completed")}
+            tasks.append(task)
+        info[user_id] = tasks
+
+    with open('todo_all_employees.json', 'w+') as file:
+        file.write(json.dumps(info))
+
+     
+
+if __name__ == "__main__":
+    funct()
