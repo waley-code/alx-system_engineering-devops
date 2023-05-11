a336d9ba67c56558f55aa9c9504c2fdaa56c4c68
waley-code <walephlp@gmail.com>
2023-05-11 00:33:42 +0000

    update

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,8 @@
-# check spelling error in file
+#check spelling error in file
 file { '/var/www/html/wp-settings.php':
 	ensure => 'file',
 	content => file('/var/www/html/wp-settings.php')
 	.content
 	.gsub('class-wp-locale.phpp', 'class-wp-locale.php'),
-}
+	}
+
